c7702842ee6bf9a973f374a07cd7045e531a874c
brunnolou <bsl@seegno.com>
2016-12-09 20:43:19 +0000

    Add Redux to App component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
 import './App.css';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { loadState, saveState } from './core/localStorage';
 import MapGeoJson from './containers/MapGeoJson';
 import React, { Component } from 'react';
+import rootReducer from './reducers';
+
+// Load initial state from localStorage.
+const persistedState = loadState();
+
+/**
+ * Create store.
+ */
+
+const store = createStore(
+  rootReducer,
+  persistedState,
+  // eslint-disable-next-line
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+store.subscribe(() => {
+  saveState(store.getState());
+});
 
 /**
  * Export `App` Component.
@@ -9,7 +31,9 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <MapGeoJson />
+      <Provider store={store}>
+        <MapGeoJson />
+      </Provider>
     );
   }
 }
